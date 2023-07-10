b1ba97e412a96eb8f2c3bb9145936f0ad7acebeb
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-10 13:02:32 +0200

    add Base structure and menu data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
-<template></template>
+<template>
+  <PageHeader />
+  <PageMain />
+  <PageFooter />
+  <h1>ciao</h1>
+</template>
 
 <script>
 import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import PageHeader from './components/PageHeader.vue'
+import PageMain from './components/PageMain.vue'
+import PageFooter from './components/PageFooter.vue'
+import { store } from './components/data/store.js'
 export default {
-
+  components: { PageHeader, PageMain, PageFooter },
+  data() {
+    return {
+      store,
+    }
+  },
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+@use './assets/style/vars.scss' as *;
+</style>
---
+++ src/components/PageHeader.vue
@@ -0,0 +1,104 @@
+<template>
+    <div>
+
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            menuLinks: [
+                {
+                    title: 'Home',
+                    voices: [
+                        'Start Here',
+                        'Success Story',
+                        'About Me',
+                        'About Us 01',
+                        'About Us 02',
+                        'About Us 03',
+                        'Contact Me',
+                        'Contact Us',
+                        'Purchase Guide',
+                        'Privacy Policy',
+                        'Terms of Service'
+                    ],
+                    title: 'Pages',
+                    voices: [
+                        'Start Here',
+                        'Success Story',
+                        'About Me',
+                        'About Us 01',
+                        'About Us 02',
+                        'About Us 03',
+                        'Contact Me',
+                        'Contact Us',
+                        'Purchase Guide',
+                        'Privacy Policy',
+                        'Terms of Service'
+                    ],
+                    title: 'Courses',
+                    voices: [
+                        'Start Here',
+                        'Success Story',
+                        'About Me',
+                        'About Us 01',
+                        'About Us 02',
+                        'About Us 03',
+                        'Contact Me',
+                        'Contact Us',
+                        'Purchase Guide',
+                        'Privacy Policy',
+                        'Terms of Service'
+                    ],
+                    title: 'Features',
+                    voices: [
+                        'Start Here',
+                        'Success Story',
+                        'About Me',
+                        'About Us 01',
+                        'About Us 02',
+                        'About Us 03',
+                        'Contact Me',
+                        'Contact Us',
+                        'Purchase Guide',
+                        'Privacy Policy',
+                        'Terms of Service'
+                    ],
+                    title: 'Blog',
+                    voices: [
+                        'Start Here',
+                        'Success Story',
+                        'About Me',
+                        'About Us 01',
+                        'About Us 02',
+                        'About Us 03',
+                        'Contact Me',
+                        'Contact Us',
+                        'Purchase Guide',
+                        'Privacy Policy',
+                        'Terms of Service'
+                    ],
+                    title: 'Shop',
+                    voices: [
+                        'Start Here',
+                        'Success Story',
+                        'About Me',
+                        'About Us 01',
+                        'About Us 02',
+                        'About Us 03',
+                        'Contact Me',
+                        'Contact Us',
+                        'Purchase Guide',
+                        'Privacy Policy',
+                        'Terms of Service'
+                    ],
+                }
+            ]
+        }
+    },
+}
+</script>
+<style lang="">
+    
+</style>
