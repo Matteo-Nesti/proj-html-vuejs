8a6e90f0eedcd3bd992b0cb9a5260547ad771f14
Matteo-Nesti <matteo.nesti@hotmail.com>
2023-07-11 15:28:44 +0200

    add dates section

--- src/components/PageMain.vue
+++ src/components/PageMain.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
+    <section id="dates">
+        <PageMainStats :items="dates" />
+    </section>
 
-    </div>
+    <section id="about-me">
+        <div class="container my-5">
+            <div class="row justify-content-center align-items-center text-center">
+                <div class="col-5">dad</div>
+                <div class="col-7">dad</div>
+            </div>
+        </div>
+    </section>
 </template>
 <script>
+import PageMainStats from './PageMainStats.vue'
 export default {
-
+    components: { PageMainStats },
+    data() {
+        return {
+            dates: [
+                {
+                    date: '1926',
+                    title: 'FINISHED SESSION'
+                },
+                {
+                    date: '100%',
+                    title: 'SATISFACTION RATE'
+                },
+                {
+                    date: '3092+',
+                    title: 'ENROLLED LEARNERS'
+                },
+                {
+                    date: '200',
+                    title: 'ONLINE ISTRUCTORS'
+                }
+            ]
+        }
+    },
 }
 </script>
-<style lang="">
-    
-</style>
+<style lang="scss" scoped></style>
---
+++ src/components/PageMainStats.vue
@@ -0,0 +1,32 @@
+<template >
+    <div class="container my-5">
+        <div class="row row-cols-4">
+            <div v-for="item in items" class="date-card">
+                <h2>{{ item.date }}</h2>
+                <h4>{{ item.title }}</h4>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    props: {
+        items: Array
+    }
+}
+</script>
+<style lang="scss" scoped>
+@use '../assets/style/vars.scss' as *;
+
+.date-card {
+
+    text-align: center;
+    background-image: url('../assets/img/background-pattern-grid-line-06.png');
+    background-repeat: no-repeat;
+
+    h2 {
+        font-size: 2.5rem;
+        color: $main-color;
+    }
+}
+</style>
